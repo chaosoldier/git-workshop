76e494f14cdf966bdf8166450c57a333a9be893f
ShotaOda <shota.oda@bizreah.co.jp>
2016-04-25 14:33:26 +0900

    js修正

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>Konton TinBot織り成す世界</title>
-  <link rel="stylesheet" href="css/main.css"> <script src="js/main.js"></script>
-</head>
+var param   = location.href.split("?")[1];
+var user = param.split("=")[1];
 
-<body>
-  <header>
-    <h1>Konton TinBot織り成す世界</h1>
-  </header>
-  <main>
-    <ul>
-      <li>
-        <a href="./profile/xxx.html">xxxxxx</a>
-      </li>
-      <li>
-        <a href="./profile/xxx.html">xxxxxx</li>
-      <li>
-        <a href="./profile/xxx.html">xxxxxx</a>
-      </li>
-    </ul>
-  </main>
-  <footer>
+var content = {};
 
-  </footer>
-  </body>
-</html>
+$(function () {
+  $.ajax({
+    url: "/resource/" user + '.txt',
+    success: function(data){
+      var data_array = data.split(/\r\n|\r|\n/);
+      $.each(data_array,  function () {
+     	content[this.split("=")[0]] = this.split("=")[1]
+      })
+
+      var len = data_array.length;
+    }
+  });
+});
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+
+
